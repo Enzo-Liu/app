38ed4b6ba5d1dd1d9654c694696d92e0efa86bbe
Liu Enze <enze.liu@dianping.com>
2013-08-23 18:20:19 +0800

    Initial Commit

---
+++ App/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  App
+//
+//  Created by Liu Enze on 8/23/13.
+//  Copyright (c) 2013 Liu Enze. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
